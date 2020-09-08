8925f6b22c953daecf7cfbe33b64f8fc1e44e188
AnyaChurkina <anyachurkina.spb@mail.ru>
2020-09-09 01:37:13 +0300

    update my calculator

--- calculator.py
+++ calculator.py
@@ -1,13 +1,15 @@
 a = int(input())
 b = input()
 c = int(input())
-if b =='/' and c == 0:
-    print ("Division by zero!")
-elif b =='+':
-    print (a + c)
+if b == '+':
+    print(a + c)
 elif b == '-':
-    print (a - c)
+    print(a - c)
 elif b == '*':
-    print (a * c)
-elif b != '/' and c != 0:
-    print (a / c)
+    print(a * c)
+elif b == '/' and c != 0:
+    print(a / c)
+elif b == '/' and c == 0:
+    print("Division by zero!")
+elif b != '+' or b != '-' or b != '*' or b != '/':
+    print("Uknown command! Available operations: '+','-','*','/'")
