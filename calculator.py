04df4b96b1df361275d79d180c7822eeeeee4d79
AnyaChurkina <anyachurkina.spb@mail.ru>
2020-09-11 15:04:37 +0300

    calculator versoin 2.0

--- calculator.py
+++ calculator.py
@@ -1,15 +1,21 @@
-a = int(input())
-b = input()
-c = int(input())
+a = float(input('Enter a number 1: '))
+b = input('Enter a operator: ')
+c = float(input('Enter a number 2: '))
 if b == '+':
     print(a + c)
 elif b == '-':
     print(a - c)
 elif b == '*':
     print(a * c)
+elif b == '**':
+    print(a ** c)
 elif b == '/' and c != 0:
     print(a / c)
-elif b == '/' and c == 0:
+elif b == '//' and c != 0:
+    print(a // c)
+elif b == '%' and c != 0:
+    print(a % c) 
+elif b == '/' or b == '//' or b == '%' and c == 0:
     print("Division by zero!")
-elif b != '+' or b != '-' or b != '*' or b != '/':
-    print("Uknown command! Available operations: '+','-','*','/'")
+else:
+    print("Uknown operator! Available operators: '+','-','*','/','//','%','**'")
