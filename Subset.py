050cd3ecced1ae98376fa46f219407aa324b7bc4
AnyaChurkina <anyachurkina.spb@mail.ru>
2021-03-07 19:30:01 +0300

    Change train subset

--- Subset.py
+++ Subset.py
@@ -1,13 +1,12 @@
 from pandas import read_csv
-
+from statistics import mean
 
 #  Task 2 (data subset)
-train = read_csv("https://raw.githubusercontent.com/Serfentum/bf_course/master/"
-                 "14.pandas/train.csv")
+train = read_csv("https://raw.githubusercontent.com/Serfentum/bf_course/"
+                 "master/14.pandas/train.csv")
 
-new_train = train[train["matches"] > train["matches"].mean()]
 
-train[train["matches"] > train["matches"].mean()][['pos', 'reads_all',
+train[train["matches"] > mean(train["matches"])][['pos', 'reads_all',
                                                    'mismatches', 'deletions',
                                                    'insertions']]. \
     to_csv('train_part.csv')
