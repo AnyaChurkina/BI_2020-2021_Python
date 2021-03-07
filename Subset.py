c40741e7d855325ab167b990106a18c90cc47ec4
AnyaChurkina <anyachurkina.spb@mail.ru>
2021-03-07 19:48:22 +0300

    Change Subset.py

--- Subset.py
+++ Subset.py
@@ -1,12 +1,13 @@
 from pandas import read_csv
 from statistics import mean
 
+
 #  Task 2 (data subset)
 train = read_csv("https://raw.githubusercontent.com/Serfentum/bf_course/"
                  "master/14.pandas/train.csv")
 
 
 train[train["matches"] > mean(train["matches"])][['pos', 'reads_all',
-                                                   'mismatches', 'deletions',
-                                                   'insertions']]. \
+                                                  'mismatches', 'deletions',
+                                                  'insertions']]. \
     to_csv('train_part.csv')
